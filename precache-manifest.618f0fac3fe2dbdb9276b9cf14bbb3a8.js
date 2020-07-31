d2cf2e69021920764b568da6371114bc15123596
Joao Hencke <joao.hencke@gmail.com>
2020-07-31 17:26:22 -0300

    Updates

---
+++ precache-manifest.618f0fac3fe2dbdb9276b9cf14bbb3a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b538bffa201a7f82e85befb21ea520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eabe1557ff2e1d7f4b8",
+    "url": "/static/css/main.f30f143c.chunk.css"
+  },
+  {
+    "revision": "e18e7f150a0ba330453d",
+    "url": "/static/js/2.f178688a.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/static/js/2.f178688a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eabe1557ff2e1d7f4b8",
+    "url": "/static/js/main.6987ad83.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
